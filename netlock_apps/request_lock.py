94579dde6805f15d0c785f20c38aa59b270fa2e3
p4 <mdkiley2001@gmail.com>
2023-05-06 23:52:05 -0700

    send_requests works

---
+++ netlock_apps/request_lock.py
@@ -0,0 +1,62 @@
+"""
+This client sends a packet to acquire the lock.
+"""
+
+import argparse
+import sys
+import socket
+import random
+import struct
+
+from scapy.all import sendp, get_if_list, get_if_hwaddr, Ether, IP, UDP, TCP, Packet, BitField
+import time
+
+ACQUIRE = 0
+
+class LockHeader(Packet):
+    """
+    The lock header used by the client to request a lock
+    """
+
+    #BitField(name, default_size, size)
+    fields_desc=[
+        BitField("lock_id", 0, 14),
+        BitField("action", 0, 2)
+    ]
+
+
+def get_if():
+    """
+    Get the interface eth0
+    If no interface has a name with "eth0", then exit program
+    """
+
+    iface=None
+    for i in get_if_list():
+        if "eth0" in i:
+            iface=i
+            break
+    if not iface:
+        print("Cannot find eth0 interface")
+        exit(1)
+    else:
+        return iface
+
+
+
+def main():
+
+    if len(sys.argv) < 2:
+        print("more args")
+        exit(1)
+
+    addr = socket.gethostbyname(sys.argv[1])
+    iface = get_if()
+    print("Hello")
+    pkt = Ether(src=get_if_hwaddr(iface), dst='ff:ff:ff:ff:ff:ff') / LockHeader(lock_id=0, action=ACQUIRE)
+    pkt = pkt /IP(dst=addr) / UDP(dport=7777, sport=random.randint(2000,65535))
+    print(pkt)
+    sendp(pkt, iface=iface, verbose=False)
+
+if __name__ == '__main__':
+    main()
